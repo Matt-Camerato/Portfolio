865b1ef0cb4faac574225b0c1de51259857d820e
Matt Camerato <matt.camerato@gmail.com>
2025-01-07 11:06:31 -0500

    Added Vercel tracking packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Navbar } from "./components/ui/Navbar";
 import { Scene } from "./components/canvas/Scene";
 import { Overlay } from "./components/ui/Overlay";
@@ -5,11 +7,15 @@ import { OverlayProvider } from "./components/context/OverlayContext";
 
 function App() {
   return (
-    <OverlayProvider>
-      <Navbar />
-      <Scene />
-      <Overlay />
-    </OverlayProvider>
+    <>
+      <OverlayProvider>
+        <Navbar />
+        <Scene />
+        <Overlay />
+      </OverlayProvider>
+      <SpeedInsights />
+      <Analytics />
+    </>
   );
 }
 
